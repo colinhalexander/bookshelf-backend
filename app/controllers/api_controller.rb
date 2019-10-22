51afa29036749541f5ecd7409341afa49f863fc3
colinhalexander <chalexander@icloud.com>
2019-10-22 14:57:04 -0600

    add google books api endpoint

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -6,20 +6,30 @@ class ApiController < ApplicationController
     render json: nyt_data
   end
 
-  # def google
-  #   render json: google_data
-  # end
+  def google
+    render json: google_data
+  end
 
   private
 
-  def nyt_data 
+  def nyt_data
     response = RestClient.get("https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=#{ENV['NYT_KEY']}")
     response.body
   end
 
-  # def google_data 
-  #   response = RestClient.get("")
-  #   response.body
-  # end
+  def google_data
+    query = params[:query].gsub(/\s/, "+")
+    if params[:by] != "all"
+      if params[:by] != "subject"
+        query = "in#{params[:by]}:#{query}"
+      else
+        query = "subject:#{query}"
+      end
+    end
+
+    response = RestClient.get("https://www.googleapis.com/books/v1/volumes?q=#{query}&maxResults=40&startIndex=0&key=#{ENV['GOOGLE_KEY']}")
+
+    response.body
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get '/nyt', to: 'api#nyt'
-  get '/google', to: 'api#google'
+  post '/google', to: 'api#google'
 end
